b3b8f26f7cffafb704b2b370c6f28cb99fddef02
Yunusade <yunus.adebayo@sky.uk>
2023-11-13 11:20:10 +0000

    First commit

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // Import data and WeatherCard here
-
+import data from './data';
+import WeatherCard from './components/WeatherCard';
 
 function App() {
     return (
@@ -9,8 +10,9 @@ function App() {
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
                 {/* Render components here */}
-                
-
+                {data.map((day, index) => {
+                    return <WeatherCard key={index} day={day} />
+                })}
 
             </div>
             
